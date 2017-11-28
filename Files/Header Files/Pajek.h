2def2e207868a8c83b58a24b327411e6db12aede
Alvaro Oliveira <alvaro_s_oliveira@hotmail.com>
2017-11-28 01:53:54 -0200

    BUGFIX - *Edges(gravação bidirecional) - PAJEK 100%

--- Files/Header Files/Pajek.h
+++ Files/Header Files/Pajek.h
@@ -4,11 +4,14 @@
 #include <sstream>
 #include <iterator>
 #include <algorithm>
+#include <tuple>
 
 class Pajek
 {
 public:
-	Pajek();
+	Pajek() {
+
+	};
 
 	//SPACE SPLIT TEMPLATE
 	template<typename Out>
@@ -24,16 +27,12 @@ public:
 		split(s, delim, back_inserter(elems));
 		return elems;
 	}
-	/*
-	//CRIA EDGES -> NAO DIRECIONADO
-	static void edges(Grafo * g,int i,int j,float p) {
-		g->adj->cria_adjacencia(i, j, p);
-		g->adj->cria_adjacencia(j, i, p);
+
+	//MODULO DE IMPORTACAO DE BASE DE DADOS
+	static void importa_pajek(Grafo * g) {
+
 	}
-	//CRIA ARCS -> DIRECIONADO
-	static void arcs(Grafo * g,int i, int j, float p) {
-		g->adj->cria_adjacencia(i, j, p);
-	}*/
+
 	//MODULO DE GRAVACAO PAJEK
 	static void gravacao(Grafo * g) {
 		string filename;
@@ -42,7 +41,7 @@ public:
 		filename.append(".paj");
 		ofstream file;
 
-		file.open(filename,ofstream::out);
+		file.open(filename, ofstream::out);
 		if (file.is_open() == false) {
 			cout << "Erro ao abrir o arquivo: " << filename << endl;
 			exit(0);
@@ -53,17 +52,49 @@ public:
 		for (int i = 0; i < g->size; i++) {
 			file << (i + 1) << " \"" << g->adj->vertices[i].nome << "\"\n\n";
 		}
-		if (g->adj->direcional()) {
+		if (!g->adj->direcional()) {
 			file << "*Arcs" << "\n\n";
+			for (int i = 0; i < g->size; i++) {
+				for (vector<NoRef>::iterator it = g->adj->vertices[i].adjacentes.begin(); it != g->adj->vertices[i].adjacentes.end(); it++) {
+					file << i + 1 << " " << it->node->cod + 1 << " " << it->peso << "\n\n";
+				}
+			}
 		}
 		else {
 			file << "*Edges" << "\n\n";
-		}
-		for (int i = 0; i < g->size; i++) {
-			for (vector<NoRef>::iterator it = g->adj->vertices[i].adjacentes.begin(); it != g->adj->vertices[i].adjacentes.end(); it++) {
-				file << i + 1 << " " << it->node->cod+1 << " " << it->peso << "\n\n";
+			vector<tuple<int, int, float>> coords;
+			for (int i = 0; i < g->size; i++) {
+				for (vector<NoRef>::iterator it = g->adj->vertices[i].adjacentes.begin(); it != g->adj->vertices[i].adjacentes.end(); it++) {
+					//file << i + 1 << " " << it->node->cod + 1 << " " << it->peso << "\n\n";
+					coords.push_back(make_tuple(i, it->node->cod, it->peso));
+				}
+			}
+			/* DEBUG TUPLA ANTES DO REMOVE
+			for (auto it = coords.begin(); it != coords.end(); ++it) {
+			cout << "TUPLA ANTES DO REMOVE" << endl;
+			cout << get<0>(*it) << " " << get<1>(*it) << " " << get<2>(*it) << endl;
+			}*/
+
+			int coord1 = 0, coord2 = 0, coord3 = 0, coord4 = 0;
+			float peso1 = 0, peso2 = 0;
+			for (int it = 0; it < coords.size(); ++it) {
+				for (int jt = 0; jt < coords.size(); ++jt) {
+					tie(coord1, coord2, peso1) = coords[it];
+					tie(coord3, coord4, peso2) = coords[jt];
+					if (coord1 == coord4 && coord2 == coord3) {
+						coords.erase(coords.begin() + jt);
+					}
+				}
+			}
+
+			for (auto it = coords.begin(); it != coords.end(); ++it) {
+				//cout << "TUPLA DEPOIS DO REMOVE" << endl;
+				//cout << get<0>(*it) << " " << get<1>(*it) << " " << get<2>(*it) << endl;
+				//INSERIR + 1 POR CONTA DO FORMATO PAJEK QUE INICIA EM 1
+				file << get<0>(*it) + 1 << " " << get<1>(*it) + 1 << " " << get<2>(*it) << "\n\n";
 			}
 		}
+
 		file.close();
 	}
 	//MODULO DE CARREGAMENTO PAJEK
@@ -133,8 +164,8 @@ public:
 			}
 			else {
 				//CHAMA ARCS
-				Grafo * g1 = new Grafo(tempsize,false);
-				for (int i = 0; i < vertices_temp.size();i++) {
+				Grafo * g1 = new Grafo(tempsize, false);
+				for (int i = 0; i < vertices_temp.size(); i++) {
 					g1->adj->seta_informacao(i, vertices_temp[i]);
 				}
 				do {
@@ -145,8 +176,8 @@ public:
 					vector<int> tokens;
 					float token_peso = 0;
 					tokens_split = split(linha, ' ');
-					tokens.resize(tokens_split.size()-1);
-					for (int i = 0; i < tokens_split.size()-1; i++) {
+					tokens.resize(tokens_split.size() - 1);
+					for (int i = 0; i < tokens_split.size() - 1; i++) {
 						tokens[i] = stoi(tokens_split[i]);
 					}
 					token_peso = stof(tokens_split[2]);
@@ -175,9 +206,9 @@ public:
 				vector<string> tokens_split;
 				vector<int> tokens;
 				float token_peso = 0;
-				tokens_split = split(linha,' ');
-				tokens.resize(tokens_split.size()-1);
-				for (int i = 0; i < tokens_split.size()-1; i++) {
+				tokens_split = split(linha, ' ');
+				tokens.resize(tokens_split.size() - 1);
+				for (int i = 0; i < tokens_split.size() - 1; i++) {
 					tokens[i] = stoi(tokens_split[i]);
 				}
 				token_peso = stof(tokens_split[2]);
@@ -196,16 +227,18 @@ public:
 	//VERIFICA DIRECIONAL
 	/*
 	bool direcional(Grafo * g) {
-		for (int i = 0; i < g->size; i++) {
-			for (vector<NoRef>::iterator it = g->adj->vertices[i].adjacentes.begin(); it != g->adj->vertices[i].adjacentes.end(); it++) {
-				for (vector<NoRef>::iterator jt = g->adj->vertices[it->node->cod].adjacentes.begin(); jt != g->adj->vertices[it->node->cod].adjacentes.end(); jt++) {
-					if (it->peso != jt->peso) { return true; }
-				}
-			}
-		}
-		return false;
+	for (int i = 0; i < g->size; i++) {
+	for (vector<NoRef>::iterator it = g->adj->vertices[i].adjacentes.begin(); it != g->adj->vertices[i].adjacentes.end(); it++) {
+	for (vector<NoRef>::iterator jt = g->adj->vertices[it->node->cod].adjacentes.begin(); jt != g->adj->vertices[it->node->cod].adjacentes.end(); jt++) {
+	if (it->peso != jt->peso) { return true; }
+	}
+	}
+	}
+	return false;
 	}*/
 
-	~Pajek();
+	~Pajek() {
+
+	};
 };
 
